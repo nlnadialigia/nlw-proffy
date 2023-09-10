703f8b09b141b53d2deca29f1a06b9e50399c98e
Nadia Ligia <nlnadialigia@gmail.com>
2023-09-10 00:15:19 -0300

    create teacher list page

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import {Link} from 'react-router-dom';
+import backIcon from "../../assets/images/icons/back.svg";
+import logoImg from "../../assets/images/logo.svg";
+import './styles.css';
 
 const TeacherList: React.FC = () => {
-  return <h1>Teacher</h1>;
+  return (
+    <div id="page-teacher-list" className='container'>
+      <header className="page-header">
+        <div className="top-bar-container">
+          <Link to="/" >
+            <img src={backIcon} alt="Ícone de voltar" />
+          </Link>
+          <img src={logoImg} alt="Logo Proffy" />
+        </div>
+        <div className="header-content">
+          <strong>Estes são os professores disponíveis.</strong>
+        </div>
+      </header>
+    </div>
+  )
 }
 
 export default TeacherList;

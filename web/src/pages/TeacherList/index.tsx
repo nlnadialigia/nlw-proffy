71c0ff41afbac9496e1e35b47c8ff6cf5662fccd
Nadia Ligia <nlnadialigia@gmail.com>
2023-09-10 21:05:27 -0300

    create input component

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 import './styles.css';
@@ -9,18 +10,9 @@ const TeacherList: React.FC = () => {
     <div id="page-teacher-list" className='container'>
       <PageHeader title={"Estes são os professores disponíveis."}>
         <form action="" id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" name="subject" id="subject" />
-          </div>
-          <div className="input-block">
-            <label htmlFor="week-day">Dia da semana</label>
-            <input type="text" name="week-day" id="week-day" />
-          </div>
-          <div className="input-block">
-            <label htmlFor="time">Horas</label>
-            <input type="text" name="time" id="time" />
-          </div>
+          <Input name={'subject'} label={'Matéria'} />
+          <Input name={'week_day'} label={'Dia da semana'} />
+          <Input name={'time'} label={'Horas'} />
         </form>
       </PageHeader>
       <main>
